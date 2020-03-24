6309188e063b694a50f75da0f81a55cfccf777af
laosha <zhenyu.sha@gmail.com>
2020-03-23 19:30:57 -0700

    correct the package

--- java/src/zhenyu/sha/leetcode/q568/Solution.java
+++ java/src/zhenyu/sha/leetcode/q568/Solution.java
@@ -1,69 +1,48 @@
 package zhenyu.sha.leetcode.q568;
-import java.util.*;
+
+import java.util.Arrays;
+
 class Solution {
-    static class Station implements Comparable<Station>{
-        int position;
-        int gas;
+    public int maxVacationDays(int[][] flights, int[][] days) {
+        //state per city , vacantion cumlates
 
-        public Station(int position, int gas) {
-            this.position = position;
-            this.gas = gas;
-        }
-
-        @Override
-        public int compareTo(Station o) {
-            return this.position-o.position;
-        }
-    }
-
-    public int minRefuelStops(int target, int startFuel, int[][] stations) {
-        //state ,  cost,  left gas
-        Map<Integer, Integer>dp1 = new HashMap<>();
-        Map<Integer, Integer>dp2 = new HashMap<>();
-
-        List<Station> stationList = new ArrayList<>();
-        for(int i =0; i<stations.length;i++) {
-            stationList.add(new Station(stations[i][0], stations[i][1]));
-        }
-        stationList.add(new Station(target,0));
-        Collections.sort(stationList);
-        dp1.put(0, startFuel);
-        int lastPos = 0;
-        for (Station s: stationList) {
-            int currentPos = s.position;
-            int curGas = s.gas;
-            for(Map.Entry<Integer, Integer> entry: dp1.entrySet()){
-                int leftGas = entry.getValue();
-                int cost = entry.getKey();
-                if(leftGas>=currentPos-lastPos) {
-                    // not fill it
-                    int newLeft = leftGas-(currentPos-lastPos);
-                    int newCost = cost;
-                    if(newLeft>dp2.getOrDefault(newCost, Integer.MIN_VALUE)){
-                        dp2.put(newCost, newLeft);
-                    }
-                    //fill it
-                    newLeft +=curGas;
-                    newCost++;
-                    if(newLeft>dp2.getOrDefault(newCost, Integer.MIN_VALUE)){
-                        dp2.put(newCost, newLeft);
+        int N = flights.length;
+        int K = days[0].length;
+        int curDP =0;
+        int dp[][]= new int[2][N+1];
+        Arrays.fill(dp[0], -1);
+        Arrays.fill(dp[1], -1);
+        dp[curDP][0]=0;
+        //for days
+        for(int i=0;i<K;i++) {
+            int nextDP = (curDP+1)%2;
+            // for cities
+            for(int c =0; c<N; c++) {
+                if(dp[curDP][c]>-1) {
+                    int cum = dp[curDP][c];
+                    for(int n=0;n<N;n++) {
+                        if(c==n||flights[c][n]==1){
+                            if (dp[nextDP][n]<cum+days[n][i]){
+                                dp[nextDP][n]=cum+days[n][i];
+                            }
+                        }
                     }
                 }
             }
-            if(dp2.size()==0)
-                return -1;
-            lastPos=currentPos;
-            Map<Integer, Integer> temp = dp1;
-            dp1=dp2;
-            dp2=temp;
-            dp2.clear();
+            //swap dp status
+            Arrays.fill(dp[curDP],-1);
+            curDP = nextDP;
         }
-        int ret =Integer.MAX_VALUE;
-        for(int cost:dp1.keySet()){
-            if(cost<ret) {
-                ret = cost;
+        // CheckMax
+        int ret =0;
+        for(int i =0; i<N;i++) {
+            if(dp[curDP][i]>ret){
+                ret = dp[curDP][i];
             }
         }
         return ret;
+    }
+    public static void main(String[]args){
+        System.out.println(new Solution().maxVacationDays(new int[][]{{0,1,1},{1,0,1},{1,1,0}}, new int[][]{{1,3,1},{6,0,3},{3,3,3}}));
     }
 }
