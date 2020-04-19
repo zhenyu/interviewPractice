61699b191bd4a0c65a7344434b563a97dfe2c918
laosha <zhenyu.sha@gmail.com>
2020-04-18 22:08:24 -0700

    自己手打了一边913

--- java/src/zhenyu/sha/leetcode/q913/Solution.java
+++ java/src/zhenyu/sha/leetcode/q913/Solution.java
@@ -2,131 +2,89 @@ package zhenyu.sha.leetcode.q913;
 
 import java.util.*;
 class Solution {
-    static class State {
-        boolean mouse;
-        int mPos;
-        int cPos;
-
-        public State(boolean mouse, int mPos, int cPos) {
-            this.mouse = mouse;
-            this.mPos = mPos;
-            this.cPos = cPos;
-        }
-
-        @Override
-        public String toString() {
-            return "State{" +
-                    "mouse=" + mouse +
-                    ", mPos=" + mPos +
-                    ", cPos=" + cPos +
-                    '}';
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            State state = (State) o;
-            return mouse == state.mouse &&
-                    mPos == state.mPos &&
-                    cPos == state.cPos;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(mouse, mPos, cPos);
+    class State {
+        //mouse position
+        int m;
+        // cat position
+        int c;
+        // whose turn
+        int t;
+        public State(int m, int c, int t) {
+            this.m = m;
+            this.c = c;
+            this.t = t;
         }
     }
-    // return 1 if the game is won by Mouse, 2 if the game is won by Cat, and 0 if the game is a draw.
-    // 2 mouse win, 1 draw, 0 catWin
-    Map<State, Integer> seenStates = new HashMap<>();
+    final int DRAW = 0, MOUSE = 1, CAT = 2;
     public int catMouseGame(int[][] graph) {
-        seenStates.clear();
-        State initState = new State(true, 1, 2);
-        Set<State> paths = new HashSet<>();
-        int score = dfs(initState, graph, 0, paths);
-        if(score==1){
-            //draw
-            score = 0;
-        } else if(score==0) {
-            // cat win
-            score =2;
-        } else if(score==2) {
-            // mouse win
-            return 1;
-        }
-        //System.out.println("score="+score);
-        return score;
-    }
-    int dfs(State state,int[][] graph , int dep, Set<State> paths ){
-        String prefix ="";
-        for(int i = 0; i<dep;i++) {
-            prefix=prefix+"    ";
-        }
+        int N = graph.length;
 
-        Integer ret = seenStates.get(state);
-        if(null!=ret){
-            System.out.println(prefix+ "cached "+state+ret);
-            return ret;
-        }
-        if(state.mPos==0) {
-            seenStates.put(state, 2);
-            System.out.println(prefix+state.toString()+2);
-            return 2;
-        }
-        if(state.mPos==state.cPos) {
-            seenStates.put(state, 0);
-            System.out.println(prefix+state.toString()+0);
-            return 0;
-        }
-        paths.add(state);
-        if(state.mouse) {
-            ret = Integer.MIN_VALUE;
-            for(int nextPos : graph[state.mPos]) {
-                State nextState = new State(false, nextPos, state.cPos);
-                System.out.println(prefix+ "visiting "+nextState);
-                //default
-                int nextScore = 1;
-                if(!paths.contains(nextState)) {
-                    nextScore= dfs(nextState, graph, dep+1, paths);
-                }
-                ret = Math.max(ret,nextScore);
-                System.out.println(prefix+state.toString()+ret);
-                if(ret ==2){
-                    break;
-                }
-
-            }
-
-        }
-        else {
-            ret = Integer.MAX_VALUE;
-            for(int nextPos : graph[state.cPos]) {
-                State nextState = new State(true, state.mPos, nextPos);
-                if(nextPos!=0&&(!paths.contains(nextState))) {
-                    System.out.println(prefix+ "visiting "+nextState);
-                    int nextScore= dfs(nextState, graph, dep+1,paths);
-                    ret = Math.min(ret,nextScore);
-                    System.out.println(prefix+state.toString()+ret);
-                    if(ret ==0){
-                        break;
+        int[][][] color = new int[N][N][3];
+        int[][][] degree = new int[N][N][3];
+        //init the win lose state
+        LinkedList<State> finalizedStates = new LinkedList<>();
+        for(int m=0; m<N;m++) {
+            for(int c=0;c<N;c++) {
+                for(int t=1;t<=2;t++) {
+                    if(c==0) {
+                        //猫不能在 0
+                        continue;
+                    }
+                    if(m==c) {
+                        color[m][c][t]=CAT;
+                        finalizedStates.add(new State(m, c, t));
+                    }
+                    if(0==m) {
+                        color[m][c][t]= MOUSE;
+                        finalizedStates.add(new State(m, c, t));
+                    }
+                    //degree
+                    degree[m][c][MOUSE]=graph[m].length;
+                    degree[m][c][CAT]=graph[c].length;
+                    for(int p:graph[c]){
+                        if(p==0){
+                            degree[m][c][CAT]--;
+                        }
                     }
                 }
             }
-            if(Integer.MAX_VALUE==ret){
-                ret =1;
+        }
+        while (!finalizedStates.isEmpty()) {
+            State curState = finalizedStates.remove();
+            for (State father: fathers(graph, curState.m, curState.c, curState.t)) {
+                // 否者剪掉degree
+                degree[father.m][father.c][father.t]--;
+                //上一步就是想这个颜色
+                if(father.t==color[curState.m][curState.c][curState.t]) {
+                    color[father.m][father.c][father.t]=color[curState.m][curState.c][curState.t];
+                } else{
+                    if(degree[father.m][father.c][father.t]==0){
+                        //之前没上色，就是活命其他的都是输了
+                        if(DRAW==color[father.m][father.c][father.t]){
+                            color[father.m][father.c][father.t]=3-color[curState.m][curState.c][curState.t];
+                        }
+                    }
+                }
             }
         }
-        paths.remove(state);
-
-        seenStates.put(state, ret);
-
-        System.out.println(prefix +"return "+state.toString()+ret);
-        return  ret;
+        return color[1][2][MOUSE];
     }
 
-    public static void main(String[] args) {
-        int[][]graph = new int[][]{{6},{4},{9},{5},{1,5},{3,4,6},{0,5,10},{8,9,10},{7},{2,7},{6,7}};
-        System.out.println(new Solution().catMouseGame(graph));
+    // What nodes could play their turn to
+    // arrive at node (m, c, t) ?
+    public List<State> fathers(int[][] graph, int m, int c, int t) {
+        List<State> fathers = new LinkedList<>();
+        if(t==CAT) {
+            //previous is MOUSE move ,
+            for(int father: graph[m]){
+                fathers.add(new State(father, c, MOUSE));
+            }
+        } else {
+            //previous is CAT
+            for(int father:graph[c]){
+                fathers.add(new State(m, father, CAT));
+            }
+        }
+        return fathers;
     }
 }
