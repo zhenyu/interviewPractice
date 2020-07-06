762cf080ccbf5591c882a46de8eb5458cfaca5aa
laosha <zhenyu.sha@gmail.com>
2020-07-05 20:07:55 -0700

    binary search tree , two mych time

--- java/src/zhenyu/sha/leetcode/q315/Solution.java
+++ java/src/zhenyu/sha/leetcode/q315/Solution.java
@@ -1,41 +1,39 @@
 package zhenyu.sha.leetcode.q315;
 import java.util.*;
+class IndexNode implements Comparable<IndexNode>{
+    IndexNode(int index, int val){
+        this.index=index;
+        this.val=val;
+    }
+    public int compareTo(IndexNode o){
+        return Integer.compare(o.val, this.val);
+    }
+    int index;
+    int val;
+}
 class Solution {
     public List<Integer> countSmaller(int[] nums) {
         if(nums==null||nums.length==0){
             return new LinkedList<Integer>();
         }
-
-        Integer[] ret = new Integer[nums.length];
-
-        for(int i=nums.length-1;i>=0;i--){
-            int insertPoint = serchInsert(nums, i+1, nums.length-1, nums[i]);
-            int size = insertPoint-(i+1)+1;
-            ret[i]=size;
-            int temp = nums[i];
-            for(int j =i+1;j<=insertPoint;j++){
-                nums[j-1]=nums[j];
-            }
-            nums[insertPoint]=temp;
+        IndexNode [] indexNodes = new IndexNode[nums.length];
+        for(int i=0;i<nums.length;i++) {
+            indexNodes[i]= new IndexNode(i, nums[i]);
         }
+        Arrays.sort(indexNodes);
+        Integer[] ret = new Integer[nums.length];
+        TreeSet<Integer> indexSet = new TreeSet<>();
+        for(int i= indexNodes.length-1;i>=0;i--){
+            int currentIndex = indexNodes[i].index;
+            ret[currentIndex]= indexSet.tailSet(currentIndex).size();
+            indexSet.add(currentIndex);
+        }
+
         return Arrays.asList(ret);
     }
-    int serchInsert(int[]nums, int begin, int end, int target){
-        // end is open
-        if((begin>end)||(nums[begin]>=target)){
-            return begin-1;
-        }
-        if (nums[end]<target){
-            return end;
-        }
-        while (begin<end-1){
-            int mid = begin+(end-begin)/2;
-            if(nums[mid]<target){
-                begin=mid;
-            } else {
-                end = mid;
-            }
-        }
-        return begin;
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countSmaller(new int[]{5,2,6,1}));
     }
+
 }
