90b14e4acf1106877b6be8e29c63dfab1c049572
laosha <zhenyu.sha@gmail.com>
2020-07-11 10:36:05 -0700

    1240 没onlinel judge 就这样吧，先

--- java/src/zhenyu/sha/leetcode/q1240/Solution.java
+++ java/src/zhenyu/sha/leetcode/q1240/Solution.java
@@ -1,42 +1,54 @@
+package zhenyu.sha.leetcode.q1240;
+
 class Solution {
+    int count ;
+    int h;
     public int tilingRectangle(int n, int m) {
-        if(Math.min(m, n)==11&&Math.max(m,n)==13)
-            return 6;
-
-        int dp [][]= new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                if(i==j) {
-                    dp[i][j]=1;
-                } else {
-                    dp[i][j]=Integer.MAX_VALUE;
-                }
-
-
-            }
+        int count = m*n;
+        if(n<m){
+            int temp  =n;
+            n=m;
+            m= temp;
         }
-
-        return fillTiling(n, m, dp);
-
+        h = m;
+        int [] fill = new int[n];
+        dfs(fill, 0);
+        return count;
     }
 
-    private int fillTiling(int n, int m, int[][] dp) {
-
-        if (dp[n][m]==Integer.MAX_VALUE) {
-            int cost = Integer.MAX_VALUE;
-            for(int i=1;i<=n/2;i++) {
-                cost = Math.min(cost, fillTiling(n-i, m, dp)+fillTiling(i,m,dp));
-
-            }
-            for(int j=1;j<=m/2;j++){
-                cost = Math.min(cost, fillTiling(n, m-j, dp)+fillTiling(n,j,dp));
-
-            }
-            if(cost<dp[n][m]){
-                dp[n][m]=cost;
+    private void dfs(int [] fill, int curCount) {
+        if(curCount>=count){
+            return;
+        }
+        int minIndex = fill.length-1;
+        // check the height;
+        for (int i = fill.length-2;i>=0;i--){
+            if(fill[i]<=fill[minIndex]){
+                minIndex =i;
             }
         }
-        return dp[n][m];
+        if(fill[minIndex]==h){
+            count = curCount;
+            return;
+        }
+        // try the max width
+        int w =1;
+        int curH = fill[minIndex];
+        while (curH==fill[minIndex+w]&&curH+w<=h){
+            w++;
+        }
+        for(int i=w;i>0;i--) {
+            // fill the width
+            for(int j = minIndex; j<minIndex+i;j++) {
+                fill[j]+=i;
+            }
+            dfs(fill, curCount+1);
+            // remove current case
+            // fill the width
+            for(int j = minIndex; j<minIndex+i;j++) {
+                fill[j]-=i;
+            }
+        }
     }
 
 }
