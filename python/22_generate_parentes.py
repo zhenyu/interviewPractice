e6d42a71706579dc53d56bc9b6f3a5641108ed1e
Zhenyu Sha <zhenyu.sha@gmail.com>
2022-02-01 15:16:21 +0800

    22 ver1

--- python/22_generate_parentes.py
+++ python/22_generate_parentes.py
@@ -1,7 +1,10 @@
+from typing import List
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
-        self.dfs(0, 0, 0, [], result, 2*n)
+        self.dfs(0, 0, 0, [], result, 2 * n)
         return result
 
     def dfs(self, pos: int, left_cnt: int, right_cnt: int, path: str, result, max: int) -> List[str]:
@@ -10,14 +13,17 @@ class Solution:
                 result.add(path)
             return
         # try ')'
-        if left_cnt >= right_cnt+1:
+        if left_cnt >= right_cnt + 1:
             path.add(')')
-            self.dfs(pos+1, left_cnt, right_cnt+1, path, result, max)
+            self.dfs(pos + 1, left_cnt, right_cnt + 1, path, result, max)
             path.pop()
         # try '('
         path.add('(')
-        self.dfs(pos+1, left_cnt+1, right_cnt, path, result, max)
+        self.dfs(pos + 1, left_cnt + 1, right_cnt, path, result, max)
         path.pop()
-if __name__=='main':
-    for result in Solution().generateParenthesis(0):
+
+
+if __name__ == 'main':
+    results = Solution().generateParenthesis(3)
+    for result in results:
         print(result)
