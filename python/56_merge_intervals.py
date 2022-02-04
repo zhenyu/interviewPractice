6059df8d12b72119d1b5b28b6f062bc1eb9a58c6
zhenyu sha <zhenyu.sha@gmail.com>
2022-02-04 16:07:40 +0800

    sort and merge version

--- python/56_merge_intervals.py
+++ python/56_merge_intervals.py
@@ -1,47 +1,27 @@
-from  typing import List
-import heapq
-
-
-class Node:
-    def __init__(self, event_type: int, val: int):
-        self.event_type = event_type
-        self.val = val
-
-    def __lt__(self, other):
-        if self.val == other.val:
-            return self.event_type < other.event_type
-        return self.val < other.val
+from typing import List
+from operator import itemgetter
 
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # init the scan line
-        heap = []
-        for interval in intervals:
-            heapq.heappush(heap, Node(0, interval[0]))
-            heapq.heappush(heap, Node(1, interval[1]))
-
         result = []
-        count = 0
-        start = -1;
-
-        while heap:
-            event = heapq.heappop(heap)
-            # begin the interval
-            if not event.event_type:
-                if not count:
-                    start = event.val
-                count += 1
-                # end the interval
+        intervals.sort(key=lambda x: x[0])
+        prev = None
+        while intervals:
+            interval = intervals.pop(0)
+            if not prev:
+                prev = interval
+            elif prev[1] < interval[0]:
+                result.append(prev)
+                prev = interval
             else:
-                count -= 1
-                if not count:
-                    result.append([start, event.val])
+                prev = [prev[0], max(prev[1],interval[1])]
 
+        result.append(prev)
         return result
 
 
 if __name__ == '__main__':
-    intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-    for interval in Solution().merge(intervals):
-        print(interval)
+    intervals = [[2, 6], [1, 3], [8, 10], [15, 18]]
+    for i in Solution().merge(intervals):
+        print(i)
