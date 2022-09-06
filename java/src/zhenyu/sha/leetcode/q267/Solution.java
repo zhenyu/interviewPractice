c9f5b10816337b191ffa061bbc5e2b800feacdf3
zhenyu sha <zhenyu.sha@gmail.com>
2022-09-06 14:54:56 +0800

    with backtrace

--- java/src/zhenyu/sha/leetcode/q267/Solution.java
+++ java/src/zhenyu/sha/leetcode/q267/Solution.java
@@ -8,9 +8,8 @@ class Solution {
             countMap.put(c, countMap.getOrDefault(c, 0)+1);
         }
         int oddCount  = 0;
-        List<Character> permList = new ArrayList<>();
-        String single = "";
-        Character[] perm = new Character[0];
+        List<Character> chars = new ArrayList<>();
+        Character middle = null;
         for( Map.Entry<Character, Integer> entry: countMap.entrySet()){
             int value = entry.getValue();
             if (1==value%2){
@@ -18,66 +17,60 @@ class Solution {
                 if(oddCount>1){
                     return result;
                 }
-                single = single+entry.getKey();
+                middle = entry.getKey();
                 value --;
             }
             value = value/2;
             for(; value>0;value--){
-                permList.add(entry.getKey());
+                chars.add(entry.getKey());
             }
-            perm = permList.toArray(new Character[]{});
-
         }
-        Character[] cur = perm.clone();
-        while (true){
-            String newPerm  = single;
-            for(char c: cur) {
-                newPerm= c+newPerm+c;
+        Comparator<Character> characterComparator = new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o1-o2;
             }
-            result.add(newPerm);
-            Character[] next = getNextPerm(cur);
-            if(Arrays.equals(next,perm)){
-                break;
-            }
-            cur = next;
-        }
+        };
+        Character[] characters = chars.toArray(new Character[]{});
+        Arrays.sort(characters, characterComparator);
+        dfs(characters, 0, new LinkedList<Character>(), new HashSet<Integer>(), middle, result);
         return result;
+
     }
-    Character[] getNextPerm(Character[] prev){
-        if(prev.length <=1){
-            return prev;
-        }
-        Character [] next = prev.clone();
-        int i = next.length -2;
-        for(; i>=0; i--){
-            if(next[i]<next[i+1]){
-                break;
+    void dfs(  Character[] chars, int level, LinkedList<Character> path, Set<Integer> seenPos,  Character middle, List<String> result) {
+        if(level>=chars.length) {
+            StringBuilder sb = new StringBuilder();
+            if (null!= middle) {
+                sb.append(middle);
             }
+
+            for(char c: path) {
+                sb.append(c);
+                sb.insert(0, c);
+            }
+            result.add(sb.toString());
+            return;
         }
-        // swap
-        if(i>=0){
-            for(int j= next.length-1;j > i; j--){
-                if(next[j]>next[i]){
-                    //swap
-                    char temp  = next[i];
-                    next[i] = next[j];
-                    next [j] = temp;
-                    break;
+
+        int pos =0;
+        while (pos<chars.length) {
+            if (!seenPos.contains(pos)){
+                path.add(chars[pos]);
+                seenPos.add(pos);
+                dfs(chars, level+1, path, seenPos, middle, result);
+                path.removeLast();
+                seenPos.remove(pos);
+                do {
+                    pos++;
                 }
+                while (pos<chars.length&&chars[pos]==chars[pos-1]);
+            } else {
+                pos++;
             }
         }
-        // reverse i ;
-        i++;
-        int j= next.length-1;
-        while (i<j){
-            char temp  = next[i];
-            next[i] = next[j];
-            next [j] = temp;
-            i++;
-            j--;
-        }
-        return next;
+
     }
+
     public static void main(String[] args) {
         System.out.println(new Solution().generatePalindromes("aabbcc"));
     }
