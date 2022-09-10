a18cce6f2a193ff006e8f671acfc3b5477eabc37
zhenyu sha <zhenyu.sha@gmail.com>
2022-09-10 14:44:38 +0800

    simplify version

--- java/src/zhenyu/sha/leetcode/q406/Solution.java
+++ java/src/zhenyu/sha/leetcode/q406/Solution.java
@@ -1,35 +1,26 @@
 package zhenyu.sha.leetcode.q406;
 import java.util.*;
-class Node implements Comparable<Node> {
-    int h;
-    int i;
-    Node(int h, int i) {
-        this.h=h;
-        this.i=i;
-    }
-    @Override
-    public int compareTo(Node o) {
-        int ret = this.h-o.h;
-        if(0==ret){
-            ret = o.i-this.i;
-        }
-        return ret;
-    }
-}
+
 class Solution {
 
-    public int[][] reconstructQueue(int[][] people) {
-        ArrayList<Node> nodes = new ArrayList<>(people.length);
-        for(int i=0; i<people.length;i++){
-            nodes.add(new Node(people[i][0],people[i][1]));
-        }
-        Collections.sort(nodes);
-        int [][] ret = new int[people.length][];
-        for(Node node : nodes){
+    public int[][] reconstructQueue(int[][] peoples) {
+        Arrays.sort(peoples, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                int ret = o1[0]-o2[0];
+                if(0==ret){
+                    ret = o2[1]-o1[1];
+                }
+                return ret;
+            }
+        });
+
+        int [][] ret = new int[peoples.length][];
+        for(int [] people: peoples ){
             int largerCount =0;
             for(int i=0;i<ret.length;i++) {
-                if(largerCount==node.i&&ret[i]==null){
-                    ret[i]=new int[]{node.h, node.i};
+                if(largerCount==people[2]&&ret[i]==null){
+                    ret[i]=people;
                     break;
                 }
                 if(null==ret[i]){
