4497c8b21827621fa96255bf3fea260ba3327168
zhenyu sha <zhenyu.sha@gmail.com>
2022-12-07 10:45:08 -0800

    rewrite q44

--- java/src/zhenyu/sha/leetcode/q44/Solution.java
+++ java/src/zhenyu/sha/leetcode/q44/Solution.java
@@ -1,29 +1,31 @@
 package zhenyu.sha.leetcode.q44;
 
 class Solution {
-    public boolean isMatch(String s, String p) {
-        boolean [][] matches = new boolean[p.length()+1][s.length()+1];
-        matches[0][0]=true;
-        for(int i=1;i<=p.length();i++){
-            for (int j=0;j<=s.length();j++) {
-                if(p.charAt(i-1)=='*') {
-                    for (int k=0; k<=j; k++) {
-                        if (matches[i-1][k]) {
-                            matches[i][j]=true;
+    public boolean isMatch(String source, String pattern) {
+        char[] s = source.toCharArray();
+        char [] p = pattern.toCharArray();
+        boolean[][] dp = new boolean[p.length+1][s.length+1];
+        dp[0][0] = true;
+        for(int i=1; i<p.length+1;i++) {
+            for(int j=0;j<s.length+1;j++) {
+                if(p[i-1]== '*'){
+                    // try to iterate
+                    for(int k=j;k>=0;k--){
+                        // what if all *; maybe trim it
+                        if(dp[i-1][k]) {
+                            dp[i][j] = true;
                             break;
                         }
                     }
-
-                } else if (j>0 && (p.charAt(i-1)=='?'||p.charAt(i-1)==s.charAt(j-1))){
-                    matches[i][j] = matches[i-1][j-1];
+                } else if(j>0&&(p[i-1]=='?'||p[i-1]==s[j-1])) {
+                    dp[i][j]=dp[i-1][j-1];
                 }
             }
         }
-        return matches[p.length()][s.length()];
+        return dp[p.length][s.length];
     }
-    public static void main(String[]args){
-        //"adceb"
-        //"*a*b"
-        System.out.println(new Solution().isMatch("adceb","*a*b"));
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isMatch("adceb", "*a*b"));
     }
 }
