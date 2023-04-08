5f1de5f8cbd564fe8086e3948ea306a6c31fc2db
zhenyu.sha <zhenyu.sha@gmail.com>
2023-04-08 15:27:15 -0700

    query tested

--- python/hrt_query.py
+++ python/hrt_query.py
@@ -1,13 +1,12 @@
 from typing import *
 class Solution:
    
-   
     def __init__(self, a:List[int], b:List[int]) -> None:
         self.b = b
         self.a = a
-        self.value_to_index = {} 
+        self.value_to_index = {}
         for i in range(len(a)):
-            self.value_to_index.setdefault(a[i], {}).add(i)
+            self.value_to_index.setdefault(self.a[i], set()).add(i)
         
     def query(self, q:List[int])->int:
         ret =0
@@ -15,10 +14,23 @@ class Solution:
             old_value = self.a[q[1]]
             self.value_to_index.get(old_value).remove(q[1])
             self.a[q[1]]=q[2]
-            self.value_to_index.setdefault(a[q[1]], {}).add(q[1])
+            self.value_to_index.setdefault(self.a[q[1]], set()).add(q[1])
         else:
             for i in range(len(self.b)):
-                v2s= self.value_to_index.getdefault(q[1]-self.b[i], {})
+                v2s= self.value_to_index.get(q[1]-self.b[i])
                 ret+=len(v2s)
         return ret
-                
+
+if __name__ == '__main__':
+    s = Solution([1,2,3,4,5], [6,7,8,9])
+    print(s.query([1,10]))
+    
+    print(s.query([0,4,9]))
+    print(s.query([1,10]))
+    
+    print(s.query([0,4,1]))
+    print(s.query([1,10]))
+
+    print(s.query([0,2,5]))
+    print(s.query([0,4,5]))
+    print(s.query([1,10]))
