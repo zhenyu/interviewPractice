ca8fe844fddb7ccf9eb57c5769c1a172f71fbd40
zhenyu.sha <zhenyu.sha@gmail.com>
2023-04-08 16:14:13 -0700

    2 fib tested

--- python/hrt_2_fib.py
+++ python/hrt_2_fib.py
@@ -1,12 +1,12 @@
 from typing import *
 class Solution(object):
-    def query(n:list[int])->List[bool]:
+    def query(self, n:list[int])->List[bool]:
         #find the max
         n_max=max(n)
         f1=0
         f2=1
         fs=[f1, f2]
-        while f1+f2<=max:
+        while f1+f2<=n_max:
             temp = f1
             f1=f2
             f2=f2+temp
@@ -16,13 +16,19 @@ class Solution(object):
         for i in range(len(fs)):
             if fs[i]*2>n_max:
                 break
-            for j in range(i, range(len(fs))):
+            for j in range(i, len(fs)):
+                if(fs[i]+fs[j]>n_max):
+                    break
                 p.add(fs[i]+fs[j])
         
         ret=[]
         for num in n:
-            if  n in p:
+            if  num in p:
                 ret.append(True)
             else:
                 ret.append(False)
         return ret
+
+if __name__ == '__main__':
+    ret = Solution().query([2, 17,11,10])
+    print (','.join([str(r) for r in ret]))
